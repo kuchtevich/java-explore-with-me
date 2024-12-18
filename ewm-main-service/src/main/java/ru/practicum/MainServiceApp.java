31916b27fcc2560b900b59dabb60d80e05f6267f
User Daria <kuchtevich@yandex.tu>
2024-12-18 12:02:09 +0300

    Исправление ошибок гит

--- ewm-main-service/src/main/java/ru/practicum/MainServiceApp.java
+++ ewm-main-service/src/main/java/ru/practicum/MainServiceApp.java
@@ -2,8 +2,10 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MainServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(MainServiceApp.class, args);
